7e63dee521b632cce25b88cbf86bd4afbb48ea1a
Kito Clark <kito_clark@flightcentre.com>
2021-05-01 19:00:00 +1000

    added temporary internal key requirement for usage

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,25 @@
 import { Router } from "express";
 import v1 from "./v1";
+import utils from "../utils";
 
 export default () => {
   const routes = Router({ mergeParams: true });
 
-  routes.use("/api/v1", v1);
+  routes.use(
+    "/api/v1",
+    utils.middleware.tryCatch((req: any, res: any, next: any) => {
+      console.log();
+      if (
+        !req.headers.internalkey ||
+        req.headers.internalkey !== process.env.INTERNAL_KEY
+      ) {
+        throw { message: "No Internal Key" };
+      }
+
+      next();
+    }),
+    v1
+  );
 
   return routes;
 };
