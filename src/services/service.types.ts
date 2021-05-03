59ab5b93ab93e60b824a3d8c93869ab979a993b1
Kito Clark <kito_clark@flightcentre.com>
2021-05-03 22:19:43 +1000

    testing

--- src/services/service.types.ts
+++ src/services/service.types.ts
@@ -1,14 +1,15 @@
 import { Db } from "../db/models/db.types";
-import { UserInstance } from "../db/models/User";
+// import { UserInstance } from "../db/models/User";
+import { AppInstance } from "../db/models/App";
 
 export interface UserService {
-  findOne: (where: any) => UserInstance;
+  findOne: (where: any) => any;
   register: (req: any) => { user: string };
 }
 
 export interface Container {
   db: Db;
   UserService: UserService;
-  currentUser: UserInstance;
+  currentUser: any;
   userSettings: any;
 }
