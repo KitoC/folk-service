babad9092e2dc5cc1a50a7ef8af9b3c7f840bdde
Kito Clark <kito_clark@flightcentre.com>
2021-05-04 21:32:57 +1000

    removed commented code

--- src/db/models/types/db.types.ts
+++ src/db/models/types/db.types.ts
@@ -14,18 +14,6 @@ import {
   UserSigninModelStatic,
 } from "./model.types";
 
-// import { AppModelStatic } from "./App";
-// import { AppSettingModelStatic } from "./AppSetting";
-// import { AppUserSettingModelStatic } from "./AppUserSetting";
-// import { OrganizationUserModelStatic } from "./OrganizationUser";
-// import { OrganizationModelStatic } from "./Organization";
-// import { OrganizationSettingModelStatic } from "./OrganizationSetting";
-// import { UserModelStatic } from "./User";
-// import { UserAppPasswordModelStatic } from "./UserAppPassword";
-// import { UserAppSettingModelStatic } from "./UserAppSetting";
-// import { UserOrganizationPasswordModelStatic } from "./UserOrganizationPassword";
-// import { UserSigninModelStatic } from "./UserSignin";
-
 export interface ModelOptionsExtended extends ModelOptions {
   encryptedFields: string[];
 }
--- src/db/models/types/index.ts
+++ src/db/models/types/index.ts
@@ -1,14 +1,2 @@
-// export * from "./App";
-// export * from "./AppSetting";
-// export * from "./AppUserSetting";
-// export * from "./OrganizationUser";
-// export * from "./Organization";
-// export * from "./OrganizationSetting";
-// export * from "./User";
-// export * from "./UserAppPassword";
-// export * from "./UserAppSetting";
-// export * from "./UserOrganizationPassword";
-// export * from "./UserSignin";
-
 export * from "./db.types";
 export * from "./model.types";
